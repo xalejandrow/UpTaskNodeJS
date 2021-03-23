b4ae55c50ba60d36b25ed6620c7121c8b7494b7c
Alew <xalejandrow@gmail.com>
2021-03-23 01:30:43 -0300

    Sección 12 - Video 38. Sanitizar Entradas de Datos antes de almacenarlos en la Base de Datos

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// importar express validator
+const { body } = require('express-validator/check');
+
 // importar el controlador
 const proyectosController = require('../controllers/proyectosController');
 
@@ -9,7 +12,9 @@ module.exports = function() {
     // Ruta para el home
     router.get('/', proyectosController.proyectosHome);// Ruta para el home
     router.get('/nuevo-proyecto', proyectosController.formularioProyecto);
-    router.post('/nuevo-proyecto', proyectosController.nuevoProyecto);
+    router.post('/nuevo-proyecto',
+        body('nombre').not().isEmpty().trim().escape(),
+        proyectosController.nuevoProyecto);
 
     return router;
 }
