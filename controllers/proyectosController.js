1406788559e1c81f54bdbe305ffe697b98c3b69d
Alew <xalejandrow@gmail.com>
2021-03-23 00:12:22 -0300

    Sección 8 - Video 27. Validación Simple de un Formulario

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -11,5 +11,26 @@ exports.formularioProyecto = (req, res) => {
 }
 
 exports.nuevoProyecto = (req, res) => {
-    res.send('Enviaste el Formulario');
+    // Enviar a la consola lo que el usuario escriba
+    // console.log(req.body);
+
+    // Validar que tengamos algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if(!nombre){
+        errores.push({'texto': 'Agrega un Nombre al Proyecto'})
+    }
+
+    // Sihay errores
+    if(errores.length > 0){
+        res.render('nuevoProyecto', {
+            nombrePagina : 'Nuevo Proyecto',
+            errores
+        })
+    }else{
+        // No hay errores
+        // Insertar en la BD
+    }
 }
