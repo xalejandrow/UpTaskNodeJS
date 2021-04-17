2f78636a30c2d8aa653866802a5cf7feba2aa52a
Alew <xalejandrow@gmail.com>
2021-04-17 16:27:57 -0300

    53. Agregando Sweet Alert arreglo error consola al llamar al home

--- public/js/modulos/proyectos.js
+++ public/js/modulos/proyectos.js
@@ -3,30 +3,34 @@ import axios from 'axios';
 
 const btnEliminar = document.querySelector('#eliminar-proyecto');
 
-btnEliminar.addEventListener('click', () =>{
-    // console.log('diste click en eliminar');
-    Swal.fire({
-        title: 'Deseas borrar este Proyecto?',
-        text: "Un Proyecto eliminado, no se puede recuperar!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, borrar',
-        cancelButtonText: 'No, Cancelar'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          Swal.fire(
-            'Proyecto Eliminado',
-            'El Proyecto se ha eliminado',
-            'success'
-          );
+if(btnEliminar){
+    btnEliminar.addEventListener('click', () =>{
+        // console.log('diste click en eliminar');
+        Swal.fire({
+            title: 'Deseas borrar este Proyecto?',
+            text: "Un Proyecto eliminado, no se puede recuperar!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Sí, borrar',
+            cancelButtonText: 'No, Cancelar'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Proyecto Eliminado',
+                'El Proyecto se ha eliminado',
+                'success'
+              );
+    
+            //   redireccionar al inicio
+            setTimeout(() => {
+                window.location.href = '/'
+            }, 3000);
+    
+            }
+          })
+    })
+}
 
-        //   redireccionar al inicio
-        setTimeout(() => {
-            window.location.href = '/'
-        }, 3000);
-
-        }
-      })
-})
+export default btnEliminar;
