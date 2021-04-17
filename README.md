ef3cb1737c43be1dd89f9f6743deae6a2aabf01c
Alew <xalejandrow@gmail.com>
2021-04-17 16:24:23 -0300

    53. Agregando Sweet Alert

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ Proyecto UpTask con Node y MVC
 Capitulo 52:
     Instalar Babel
     https://github.com/babel/babel-loader
+
+en consola:
+ npm install -D babel-loader @babel/core @babel/preset-env webpack
+  528  npm install --save concurrently
+  529  npm run watch
+  530  npm run watch
+  531  npm start
+
+Capitulo 52:
+    Instalar axios
+
+    npm install --save axios sweetalert2
+
+    diálogos de confirmación sweetalert2
+        https://sweetalert2.github.io/
---
+++ public/js/modulos/proyectos.js
@@ -0,0 +1,32 @@
+import Swal from 'sweetalert2';
+import axios from 'axios';
+
+const btnEliminar = document.querySelector('#eliminar-proyecto');
+
+btnEliminar.addEventListener('click', () =>{
+    // console.log('diste click en eliminar');
+    Swal.fire({
+        title: 'Deseas borrar este Proyecto?',
+        text: "Un Proyecto eliminado, no se puede recuperar!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, borrar',
+        cancelButtonText: 'No, Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Proyecto Eliminado',
+            'El Proyecto se ha eliminado',
+            'success'
+          );
+
+        //   redireccionar al inicio
+        setTimeout(() => {
+            window.location.href = '/'
+        }, 3000);
+
+        }
+      })
+})
